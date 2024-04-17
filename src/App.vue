fabe1c650aa14f20c99799e6ce3c26f283677c07
DionisiePulbere <dionisiepulbere@gmail.com>
2024-04-17 16:51:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  components: {
+    AppHeader,
+  },
+  data () {
+    return{
+      store,
+      queryParams: {
+        
+      }
+    }
+  },
+  methods: {
+  },
+  mounted() {
+   
+  }
+}
+
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+</template>
+
+<style lang="scss">
+@use './style/generic'
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+
+</script>
+
+<template>
+  ciao,sono l'header
+</template>
+
+<style scoped lang="scss">
+
+</style>
